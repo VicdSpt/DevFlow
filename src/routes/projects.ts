a978e1fbf41364aaa932384af4b4a68e7c826a9d
Victor dS <victordespirlet@gmail.com>
2026-05-09 16:14:24 +0200

    update

--- src/routes/projects.ts
+++ src/routes/projects.ts
@@ -1,11 +1,22 @@
-import {Hono} from 'hono'
-import {db} from '../db/client'
+import { Hono } from "hono";
+import { db } from "../db/client";
 
-const projects = new Hono()
+const projects = new Hono();
 
-projects.get('/', async (c) => {
-    const projects = await db.project.findMany()
-    return c.json({data: projects})
-})
+projects.get("/", async (c) => {
+  const projects = await db.project.findMany();
+  return c.json({ data: projects });
+});
 
-export default projects
+projects.post("/", async (c) => {
+  const body = await c.req.json();
+  const projects = await db.project.create({
+    data: {
+      name: body.name,
+      ownerId: "user-1",
+    },
+  });
+  return c.json({data: projects }, 201);
+});
+
+export default projects;
